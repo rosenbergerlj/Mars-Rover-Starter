01ef7721ec3bcd572e519fa95f735499c077a78a
Joe Rosenberger <joerosenberger@joes-air.attlocal.net>
2024-07-24 18:00:37 -0500

    Assignment 3: Updated through Test 3 7.24.2024

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,8 +5,22 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+  // TEST 1
+  test("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+  // TEST 2
+  test("constructor sets command type", function() {
+    let commandType = 'MODE_CHANGE';
+    let modeCommand = new Command('MODE_CHANGE', 'LOW_POWER');
+    expect(modeCommand.commandType).toBe(commandType);
+  });
+
+  // TEST 3
+  test("constructor sets a value passed in as the 2nd argument", function() {
+    let value = 12000;
+    let moveCommand = new Command('MOVE', 12000);
+    expect(moveCommand.value).toBe(value);
+  });
 });
