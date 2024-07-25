864e044e987138c681d4b37605a32d7cc8fefad5
Joe Rosenberger <joerosenberger@joes-air.attlocal.net>
2024-07-25 14:32:29 -0500

    Assignment 2: Tests 4 - 6; 7.25.2024

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,4 +6,23 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
+  // TEST 4
+  test("throws error if a name is NOT passes into the constructor as the first parameter", function() {
+    expect( function() { new Message();}).toThrow(new Error('Name required.'));
+  });
+
+  // TEST 5
+  test("constructor sets name", function() {
+    let name = 'Test message with two commands';
+    let message = new Message('Test message with two commands');
+    expect(message.name).toBe(name);
+  });
+
+  // TEST 6
+  test("contains a commands array passed into the constructor as the 2nd argument", function() {
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let message = new Message('Test message with two commands', commands);
+    expect(message.commands).toBe(commands);
+  });
+
 });
